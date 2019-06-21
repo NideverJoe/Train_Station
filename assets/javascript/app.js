2464dda62be062c648a1dbb10c45030aa91945b5
NideverJoe <nidever.joe@gmail.com>
2019-06-21 07:50:35 -0500

    added database coding to the app.js. need to work on appending the table rows.

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+//display live clock on the webpage that updates every second.
+
+$("#currenttime").text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+setInterval(function(){
+    var time = moment().format('dddd') + " " + moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currenttime").text(time)
+}, 1000)
+
+//initialize database
+    // Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyA4bEghElyUN-2vq2PkKjQYTEu6UDeh7_c",
+        authDomain: "fir-learning-a5b76.firebaseapp.com",
+        databaseURL: "https://fir-learning-a5b76.firebaseio.com",
+        projectId: "fir-learning-a5b76",
+        storageBucket: "fir-learning-a5b76.appspot.com",
+        messagingSenderId: "400196194527",
+        appId: "1:400196194527:web:a64f511ca81fb579"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+  
+      // Create a variable to reference the database.
+      var database = firebase.database();
+  
+      // Initial Values
+      var trainname = "";
+      var traindest = "";
+      var trainfreq = 0;
+      var trainfirsttime = "";
+  
+      // Capture Button Click
+      $("#add-user").on("click", function(event) {
+        event.preventDefault();
+  
+        // Grabbed values from text boxes
+        trainname = $("#addtrainname").val().trim();
+        traindest = $("#addtraindestination").val().trim();
+        trainfreq = $("#addtrainfrequency").val().trim();
+        trainfirsttime = $("#addtrainfirsttime").val().trim();
+  
+        // Code for handling the push
+        database.ref().push({
+          trainname: trainname,
+          traindest: traindest,
+          trainfreq: trainfreq,
+          trainfirsttime: trainfirsttime,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+  
+      });
+  
+      // Firebase watcher .on("child_added"
+      database.ref().on("child_added", function(snapshot) {
+        // storing the snapshot.val() in a variable for convenience
+        var sv = snapshot.val();
+  
+        // Console.loging the last user's data
+        console.log(sv.trainname);
+        console.log(sv.traindest);
+        console.log(sv.trainfreq);
+        console.log(sv.trainfirsttime);
+  
+        // create and append rows to table
+        $("#name-display").text(sv.name);
+        $("#email-display").text(sv.email);
+        $("#age-display").text(sv.age);
+        $("#comment-display").text(sv.comment);
+  
+        // Handle the errors
+      }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
+
+
+//submit user entry to firebase
+$("#add-user").on("click", function(event) {
+    event.preventDefault();
+
+
+  });
